887ab85f71e32087409891a196d1b75446d50e49
C A R N A D O <md.zeeshan787898@gmail.com>
2024-09-14 19:23:17 +0530

    utils code implemented and discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//  async handler using "async await" and "try catch"
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (err) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
