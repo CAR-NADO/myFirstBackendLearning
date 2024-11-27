8964b80a2cfaee0c2f82392617a3c29a60e0680e
Md Zeeshan <mdzeeshan787898@gmail.com>
2024-11-27 19:04:59 +0530

    postmant config

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,14 +16,16 @@ const registerUser = asyncHandler(async (req, res) => {
   // return response
 
   const { fullname, email, username, password } = req.body;
-  console.log({ fullname, email, username, password });
+  // console.log({ fullname, email, username, password });
   if (
     [fullname, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "fullname is required");
   }
 
-  const existedUser = User.findOne({
+  // console.log(req.files);
+
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -33,7 +35,16 @@ const registerUser = asyncHandler(async (req, res) => {
   //  req.body provide by Express
   //  this req.files provide by middlewares (Multer)
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage?.length > 0
+  ) {
+    coverImageLocalPath = req.files?.coverImage[0]?.path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
